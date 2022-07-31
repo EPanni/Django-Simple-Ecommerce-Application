166052ff8aa7e039c7e18baf98d232985072b466
Erick Panni <erickpanni@icloud.com>
2022-07-31 17:26:38 -0300

    STYLE[Product Variations]: Added variations in line

--- Product/admin.py
+++ Product/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from . import models
 
+
+class VariationInLine(admin.TabularInline):
+    model = models.Variation
+    extra = 1
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [VariationInLine]
+
+
 # Register your models here.
-admin.site.register(models.Product)
+admin.site.register(models.Product, ProductAdmin)
 admin.site.register(models.Variation)
